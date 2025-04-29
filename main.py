9866a0648012a380693a2a05b88fa6b058e9a68a
shinelin <shinelinxx@gmail.com>
2025-04-29 03:23:52 -0400

    feat(add): 删掉消息处理handler，因为已经在上游处理

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pkg.plugin.events import GroupNormalMessageReceived, PersonNormalMessageRec
 from pkg.platform.types import message as platform_message
 import xml.etree.ElementTree as ET
 import yaml, os
-from .handler_msg_type import MsgTypeHandler
+from typing import List, Optional, Type
 @register(
     name="LangBotYbProxyPlugin",
     description="Langbot元宝传话筒",
@@ -22,13 +22,19 @@ class LangBotYbProxyPlugin(BasePlugin):
         self.processing = {}  # user_id: (group_id, first_reply_time, last_reply_time)
         self.lock = asyncio.Lock()
         self.config = self._load_config()
-        # 只有解析到这些消息会转发给元宝
-        self._need_process_type = (
+        # 元宝接收的消息类型
+        self._yuanbao_process_type = [
             platform_message.WeChatForwardLink,
             platform_message.WeChatAppMsg,
             platform_message.WeChatForwardImage,
             platform_message.WeChatForwardFile,
-        )
+        ]
+        # 元宝回复的消息类型
+        self._yuanbao_reply_type = [
+            platform_message.WeChatForwardLink,
+            platform_message.Plain,
+        ]
+
     def _load_config(self):
         """优先加载config.yml，不存在则使用config-template.yml"""
         config_files = ['config.yml', 'config-template.yml']
@@ -88,6 +94,23 @@ class LangBotYbProxyPlugin(BasePlugin):
             self.ap.logger.error(f"提交处理失败 [用户:{user_id}] 错误:{str(e)}")
 
 
+    def _process_msg_filter(
+            self, 
+            message_chain: Optional[platform_message.MessageChain],
+            filter: List[Type[platform_message.MessageComponent]],
+            )->platform_message.MessageChain:
+        if message_chain is None:
+            return platform_message.MessageChain()
+        message_list = []
+        for component in message_chain:
+            if isinstance(component, platform_message.Quote):
+                for item in component.origin:
+                    if type(item) in filter:
+                        message_list.append(item)
+            elif type(component) in filter:
+                message_list.append(component)
+        return platform_message.MessageChain(message_list)
+
     async def _handle_yuanbao_reply(self, ctx: EventContext):
         """处理元宝回复"""
         try:
@@ -106,13 +129,11 @@ class LangBotYbProxyPlugin(BasePlugin):
             if group_id:
                 reply.insert(0, At(target=current_user))
             
-            self.ap.logger.info(f"yuanbao origin reply: {reply}")
-            msg_type_handler = MsgTypeHandler(self.ap.logger)
             await self.host.send_active_message(
                 adapter=self.host.get_platform_adapters()[0],
                 target_type="group" if group_id else "person",
                 target_id=group_id or current_user,
-                message= msg_type_handler.process_message(reply)
+                message= self._process_msg_filter(reply, self._yuanbao_reply_type)
             )
             self.ap.logger.info(f"成功投递回复 [用户:{current_user}]")
 
@@ -135,35 +156,26 @@ async def handle_group_message(self, ctx: EventContext):
         group_id = None \
             if ctx.event.launcher_id == ctx.event.sender_id \
                 else ctx.event.launcher_id
-            
-        # 获取原始消息链
-        original_chain = ctx.event.query.message_chain
         
         # 非元宝消息处理
         if ctx.event.sender_id != self.config["YUANBAO_ID"]:
             # 提取消息
-            msg_type_handler = MsgTypeHandler(self.ap.logger)
-            processed_chain = msg_type_handler.process_message(original_chain)
-            
-            # 检查是否包含需要处理的类型
-            has_target = any(
-                any(isinstance(msg, t) for msg in processed_chain)
-                for t in self._need_process_type
+            send_to_yuanbao_message = self._process_msg_filter(
+                ctx.event.query.message_chain,
+                self._yuanbao_process_type
             )
             
             # 仅当包含目标类型时处理
-            if has_target:
+            if len(send_to_yuanbao_message) > 0:
                 async with self.lock:
                     self.message_queue.append((
                         ctx.event.sender_id,
                         group_id,
-                        processed_chain,
+                        send_to_yuanbao_message,
                         datetime.now()
                     ))
                     self.ap.logger.info(f"接收群消息 [队列:{len(self.message_queue)}]")
                     ctx.prevent_default()  # 阻断默认行为
-            if len(processed_chain) == 0:
-                 ctx.prevent_default()  # 阻断默认行为
     except Exception as e:
         self.ap.logger.error(f"群消息接收异常: {str(e)}")
 
@@ -173,36 +185,25 @@ async def handle_private_message(self, ctx: EventContext):
     try:
         if ctx.event.sender_id == self.config["YUANBAO_ID"]:
             return await self._handle_yuanbao_reply(ctx)
-            
-        # 获取原始消息链
-        original_chain = ctx.event.query.message_chain
         
         # 非元宝消息处理
         if ctx.event.sender_id != self.config["YUANBAO_ID"]:
             # 提取消息
-            msg_type_handler = MsgTypeHandler(self.ap.logger)
-            processed_chain = msg_type_handler.process_message(original_chain)
-            # self.ap.logger.info(f"origin:{original_chain}, process:{processed_chain}")
-            # 检查是否包含需要处理的类型
-            has_target = any(
-                any(isinstance(msg, t) for msg in processed_chain)
-                for t in self._need_process_type
+            send_to_yuanbao_message = self._process_msg_filter(
+                ctx.event.query.message_chain,
+                self._yuanbao_process_type
             )
-            
             # 仅当包含目标类型时处理
-            if has_target:
+            if len(send_to_yuanbao_message) > 0:
                 async with self.lock:
-                    processed_message = processed_chain.copy()
                     self.message_queue.append((
                         ctx.event.sender_id,
                         None,
-                        processed_message,
+                        send_to_yuanbao_message,
                         datetime.now()
                     ))
                     self.ap.logger.info(f"接收私聊消息 [用户:{ctx.event.sender_id}]")
                     ctx.prevent_default()  # 阻断默认行为
-            if len(processed_chain) == 0:
-                 ctx.prevent_default()  # 阻断默认行为
 
     except Exception as e:
         self.ap.logger.error(f"私聊处理异常: {str(e)}, line:{e.__traceback__.tb_lineno}")
